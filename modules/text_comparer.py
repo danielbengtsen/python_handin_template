9ffad784677331214186ffb01f4741dac4756af8
danielbengtsen <cph-db145@cphbusiness.dk>
2021-03-10 12:20:43 +0100

    Made text_comparer.py importable in Jupyter - started testing download() and multi_download()

--- modules/text_comparer.py
+++ modules/text_comparer.py
@@ -1,7 +1,7 @@
 import requests
 from concurrent.futures import ProcessPoolExecutor
 import multiprocessing
-
+from tqdm import tqdm
 
 class NotFoundException(Exception):
     pass
@@ -13,7 +13,7 @@ class TextComparer:
 
     def download(self, url, filename):
         response = requests.get(url)
-        filename_full = 'data_folder/' + filename
+        filename_full = 'data/' + filename
 
         if response.ok:
             with open(filename_full, 'wb') as file_object:
@@ -24,11 +24,20 @@ class TextComparer:
     def multi_download(self):
         with ProcessPoolExecutor(multiprocessing.cpu_count()) as p:
             for i in range(0, len(self.url_list)):
+                print("... downloading and writing file: " + str(i+1))
                 self.download(self.url_list[i], 'book' + str(i+1) + ".txt")
+    
+    def __iter__(self):
+        pass
 
+    def __next__(self):
+        pass
 
-url_list = ["https://www.gutenberg.org/files/11/11-0.txt", "https://www.gutenberg.org/files/74/74-0.txt", "https://www.gutenberg.org/files/730/730-0.txt", "https://www.gutenberg.org/ebooks/4363.txt.utf-8", "https://www.gutenberg.org/files/84/84-0.txt",
-            "https://www.gutenberg.org/files/35/35-0.txt", "https://www.gutenberg.org/files/42108/42108-0.txt", "https://www.gutenberg.org/files/6130/6130-0.txt", "https://www.gutenberg.org/files/16/16-0.txt", "https://www.gutenberg.org/files/160/160-0.txt"]
-tc = TextComparer(url_list)
-#tc.download(url_list[0], 'book_1.txt')
-# tc.multi_download()
+    def urllist_generator(self):
+        pass
+
+    def avg_vowels(self, text):
+        pass
+
+    def hardest_read(self):
+        pass
